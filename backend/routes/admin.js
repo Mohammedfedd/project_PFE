9218449af14031ec2f263caaed3392fe325f62ea
Mohammedfedd <mohammedfeddouiisga@gmail.com>
2025-06-30 15:16:07 +0100

    added things

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,11 +1,22 @@
-import express from 'express'
+import express from 'express';
 import { isAdmin, isAuth } from '../middlewares/isAuth.js';
-import { addLectures, createCourse, deleteCourse, deleteLecture, getAllStats } from '../controller/admin.js';
+import {
+  addLectures,
+  createCourse,
+  deleteCourse,
+  deleteLecture,
+  getAllStats,
+  toggleComingSoon, 
+} from '../controller/admin.js';
 import { uploadFiles } from '../middlewares/multer.js';
+
 const router = express.Router();
-router.post('/course/new',isAuth, isAdmin ,uploadFiles ,createCourse);
-router.post('/course/:id',isAuth,isAdmin,uploadFiles,addLectures)
-router.delete("/course/:id",isAuth,isAdmin,deleteCourse)
-router.delete("/lecture/:id",isAuth,isAdmin,deleteLecture)
-router.get('/stats',isAuth,isAdmin,getAllStats)
+
+router.post('/course/new', isAuth, isAdmin, uploadFiles, createCourse);
+router.post('/course/:id', isAuth, isAdmin, uploadFiles, addLectures);
+router.delete('/course/:id', isAuth, isAdmin, deleteCourse);
+router.delete('/lecture/:id', isAuth, isAdmin, deleteLecture);
+router.get('/stats', isAuth, isAdmin, getAllStats);
+router.put('/course/:id/toggle-coming-soon', isAuth, isAdmin, toggleComingSoon);
+
 export default router;
